48966000ac58089940ca1ea048520dc72b690bd4
KorArtV <131380068+KorArtV@users.noreply.github.com>
2023-05-23 18:08:24 +0300

    Update learn_model.py

--- learn_model.py
+++ learn_model.py
@@ -13,8 +13,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 import pandas as pd
 import time
-models_info = {}
-i = 1
 
 models = [LogisticRegression(), RandomForestClassifier()]
 
@@ -26,6 +24,19 @@ X_train.drop(columns=X_train.columns[0], inplace=True)
 X_test.drop(columns=X_test.columns[0], inplace=True)
 y_train.drop(columns=y_train.columns[0], inplace=True)
 y_test.drop(columns=y_test.columns[0], inplace=True)
+
+X_train2 = pd.read_csv('X_train2.csv')
+X_test2 = pd.read_csv('X_test2.csv')
+y_train2 = pd.read_csv('y_train2.csv')
+y_test2 = pd.read_csv('y_test2.csv')
+X_train2.drop(columns=X_train2.columns[0], inplace=True)
+X_test2.drop(columns=X_test2.columns[0], inplace=True)
+y_train2.drop(columns=y_train2.columns[0], inplace=True)
+y_test2.drop(columns=y_test2.columns[0], inplace=True)
+
+models_info = {}
+models_info2 = {}
+i = 1
 
 def print_accuracy(model,i):
     start = time.time()
@@ -45,16 +56,42 @@ def print_accuracy(model,i):
       models_info['RFC time:'] = result_time
       models_info['RFC CR: '] = classification_report(y_test, y_pred)
 
+def print_accuracy2(model,i):
+    start = time.time()
+    y_pred = model.predict(X_test2)
+    conf_matrix = confusion_matrix(y_test2, y_pred)
+    class_rep =classification_report(y_test2, y_pred)
+    print(f'Model: \n{model}\n'
+          f'Confusion matrix: \n{conf_matrix}\n'
+          f'Classification report: \n{class_rep}\n'
+         )
+    result_time = time.time() - start
+    
+    if i == 1:
+      models_info2['LR time:'] = result_time
+      models_info2['LR CR: '] = classification_report(y_test2, y_pred)
+    if i == 2:
+      models_info2['RFC time:'] = result_time
+      models_info2['RFC CR: '] = classification_report(y_test2, y_pred)
+
 
 for mod in models:
   model = mod.fit(X_train, y_train)
   print_accuracy(model,i)
   i = i + 1
 
-def save_info(text):
-  with open('result.txt', "w") as file:
+i = 1
+
+for mod in models:
+  model = mod.fit(X_train2, y_train2)
+  print_accuracy(model,i)
+  i = i + 1
+
+def save_info(text, name):
+  with open(name, "w") as file:
       for key, value in text.items():
           file.write(f"{key}: {value}\n")
   print("Сохранение: result.txt")
 
-save_info(models_info)
+save_info(models_info,'result.txt')
+save_info(models_info2,'result2.txt')
